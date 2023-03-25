1c997fed7b5a0759f14fc38d59f1557314ebeab3
Eric Olsson <ejolsson@gmail.com>
2023-03-25 08:52:08 +0100

    Initial Commit

---
+++ MarvelSwiftUI/MarvelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelSwiftUIApp.swift
+//  MarvelSwiftUI
+//
+//  Created by Eric Olsson on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
