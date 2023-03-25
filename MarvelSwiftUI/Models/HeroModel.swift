51060a0d73f3ee14fda623dc94b58f5e0222323d
Eric Olsson <ejolsson@gmail.com>
2023-03-25 11:16:30 +0100

    HeroModel complete

--- MarvelSwiftUI/Models/HeroModel.swift
+++ MarvelSwiftUI/Models/HeroModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Hero: Codable, Identifiable {
+    var id: UUID
+    var name: String
+    var description: String
+    var photo: String
+    var series: String
+}
+
+struct HerosFilter: Codable {
+    var name: String
+} // for server calls
